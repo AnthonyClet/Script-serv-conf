40b50cdf45fc9f3f2ec388c092562a4691be4057
root <root@vps-a8408a97.vps.ovh.net>
2021-04-12 14:34:00 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "//////////////////////  Installation paquets APT   //////////////////////////"
+
+apt update
+apt install -y mariadb-server mariadb-client
+apt install -y php apache2 libapache2-mod-php php-mysql php-xml
+apt install -y composer vim git snapd
+
+echo "/////////////////////   config snapd + certbot   /////////////////////////"
+snap install core
+snap refresh core
+snap install --classic certbot
+
+echo "/////////////////////////   CERTBOT   //////////////////////"
+
+CERTBOT=$(ls /usr/bin | grep certbot)
+
+if [ -z "$CERTBOT" ]
+then
+	ln -s /snap/bin/certbot /usr/bin/certbot
+fi
+
+echo "////////////////////   Pull sources git   ///////////////////////////////"
+
+cd /var/www/html
+git pull origin master
+composer install
+chown -R www-data:www-data /var/www/html/
+source .env.dev
+
+echo "/////////////////////////   Test md5sum   //////////////////////////////"
+
+MD5_DEST=$(md5sum /etc/apache2/sites-available/000-default.conf | awk '{print $1}')
+MD5_SRC=$(md5sum 000-default.conf | awk '{print $1}')
+
+if [ "$MD5_DEST" != "$MD5_SRC" ]
+then
+	echo "ils sont differents donc j'ecrase le fichier"
+	cp 000-default.conf /etc/apache2/sites-available/000-default.conf
+	service apache2 restart
+fi
+
+echo "penser a taper la commande : certbot --apache"
